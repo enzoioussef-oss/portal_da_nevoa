956f8e57e068c0dbd612357909d664c08420a3b8
enzoioussef-oss <enzo.ioussef@escola.pr.gov.br>
2026-08-18 15:17:57 +0000

    okay

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 surv.addEventListener('click', () => {
     window.location.href = 'sobreviventes.html';
 });
+
+killer.addEventListener('click', () => {
+    window.location.href = 'assassinos.html';
+});
+
+map.addEventListener('click', () => {
+    window.location.href = 'mapas.html';
+});
